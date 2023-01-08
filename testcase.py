7ccaaee8d0366e5f49943549ba9c7ee79fad0c7a
zctest <1037814359@qq.com>
2023-01-08 23:40:29 +0800

    测试一下

---
+++ testcase.py
@@ -0,0 +1,14 @@
+# coding = utf-8
+# NAME: testcase
+# Author: ZC
+
+from selenium import webdriver
+# def testcase():
+#     print('开始构建Jenkins的项目')
+#     driver=webdriver.Chrome()
+#     driver.get("https://www.baidu.com")
+
+
+print('开始构建Jenkins的项目')
+driver=webdriver.Chrome()
+driver.get("https://www.baidu.com")
